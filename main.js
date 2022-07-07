aad4f77f5095588af1ad7a673baa82e8a0b4f5dc
rflmorais <rflmorais@gmail.com>
2022-07-07 17:30:59 -0300

    Create the main logic of the game

--- main.js
+++ main.js
@@ -7,13 +7,36 @@ function computerPlay() {
     return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
 
-// Play a single round of the game and return the winner
+/**
+ * It takes two arguments, a string for the player's selection and a string for the computer's
+ * selection, and returns a string that says whether the player won, lost, or tied
+ * @param playerSelection - The player's choice of rock, paper, or scissors.
+ * @param computerSelection - a string that is either 'rock', 'paper', or 'scissors'
+ * @returns The result of the round.
+ */
 function playRound(playerSelection, computerSelection) {
+    playerSelection = 'Paper'; // ! remove code
 
+    computerSelection = computerPlay();
+    playerSelection = playerSelection.toLowerCase();
+
+    if (computerSelection === playerSelection) {
+        return 'Tie Game!';
+    } else if (
+        (computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper') ||
+        (computerSelection === 'paper' && playerSelection === 'rock')
+    ) {
+        return 'You lost!'
+    } else {
+        return 'You win!'
+    }
 }
 
 // Call playRound(), keeps score and return the winner after a defined number of rounds
-function playGame() {
+// function playGame() {
 
-}
+// }
 
+
+console.log(playRound());

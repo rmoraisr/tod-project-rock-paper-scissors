a1352db4b47a4a01acf3e0af08ba3392ab72e48e
rflmorais <rflmorais@gmail.com>
2022-07-07 17:10:56 -0300

    Create the logical behind de computer play

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-// Randomly return 'Rock', 'Paper' or 'Scissors'
+/**
+ * The computerPlay function returns a random choice from the computerChoices array.
+ * @returns The computer's choice of rock, paper, or scissors.
+ */
 function computerPlay() {
-   
-}
+    computerChoices = ['rock', 'paper', 'scissors'];
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+};
 
 // Play a single round of the game and return the winner
 function playRound(playerSelection, computerSelection) {
-    
+
 }
 
 // Call playRound(), keeps score and return the winner after a defined number of rounds
 function playGame() {
-    
+
 }
 
